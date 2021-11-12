317bce0eec165b255b85b240594d92446ca44867
takumah1234 <hidaka@iplab.cs.tsukubal.ac.jp>
2021-11-12 16:38:47 +0900

    change: ボタンを押したらPayPayが自動起動する（iOSアプリ限定）

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ElevatedButton(
           child: const Text("起動する"),
           onPressed: () async {
-            const url = "https://line.me/R/pay/generateQR";
+            const url = "paypay://";
             await canLaunch(url)
                 ? await launch(url)
                 : throw 'Could not launch $url';
