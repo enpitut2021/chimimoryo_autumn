38c4e26a63956cee5785e414cd117c5906cbcecf
yoshinobc <t.980527t@gmail.com>
2021-12-13 01:08:48 +0900

    fix: 同じ店名が重複してドキュメントとして存在してしまうバグを解消

--- python_scripts/insert_data.py
+++ python_scripts/insert_data.py
@@ -7,6 +7,12 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 
+def set_coupon(document, pay_name, benefits):
+    coupon_doc_ref = document.collection("Payes").document()
+    coupon_doc_ref.set({"pay_name": pay_name, "benefits": float(benefits[0])})
+    print("insert coupon doc", coupon_doc_ref)
+
+
 def insert_data(datas, collection_name):
     # datas: List[data, data,...,data]
     # data: [pay_name, genre, store_name, benefits, upper_bounds, lower_bounds, target_or_other]
@@ -20,19 +26,31 @@ def insert_data(datas, collection_name):
         pay_name = data[0]
         store_name = data[2]
         benefits = re.findall(r"\d+", data[3])
+
         print(f"pay_name:{pay_name}, store_name:{store_name}, benefits:{benefits}")
         if len(benefits) != 1:
             print("benefits infomation is unexpected")
             print(data)
             continue
 
-        store_doc_ref = db.collection(collection_name).document()
-        store_doc_ref.set({"store_name": store_name})
-        print("insert store dic", store_doc_ref)
+        current_doc = (
+            db.collection(collection_name).where("store_name", "==", store_name).get()
+        )
+        if (
+            len(current_doc) == 1
+        ):  # `collection_name`コレクション内のドキュメントに新たに追加する`store_name`が既に存在する
+            print("insert store doc", current_doc[0])
+            set_coupon(current_doc[0].reference, pay_name, benefits)
+        elif len(current_doc) == 0:
 
-        coupon_doc_ref = store_doc_ref.collection("Pays").document()
-        coupon_doc_ref.set({"pay_name": pay_name, "benefits": float(benefits[0])})
-        print("insert coupon dic", coupon_doc_ref)
+            store_doc = db.collection(collection_name).document()
+            store_doc.set({"store_name": store_name})
+            print("insert store doc", store_doc)
+
+            set_coupon(store_doc, pay_name, benefits)
+
+        else:
+            raise RuntimeError("collection内に同じstore_nameのドキュメントが複数存在している")
 
 
 def get_data(csv_file_path):
